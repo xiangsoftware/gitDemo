5b34a0de251cf18febf05f364c07dbbfa046716a
chenxianguo <chenxianguo@wtoip.com>
2017-07-17 14:01:20 +0800

    分支测试。。。。。

--- src/main/java/com/demo/AAA.java
+++ src/main/java/com/demo/AAA.java
@@ -4,4 +4,8 @@ package com.demo;
  * Created by Administrator on 2017/7/17 0017.
  */
 public class AAA {
+
+    public static void main(String args[]){
+        System.out.println("================");
+    }
 }
