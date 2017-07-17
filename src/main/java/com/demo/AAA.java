5c039f72ef0f8cac705ab4518d65594a211e8429
chenxianguo <chenxianguo@wtoip.com>
2017-07-17 13:57:57 +0800

    分支测试。。。。。

--- src/main/java/com/demo/AAA.java
+++ src/main/java/com/demo/AAA.java
@@ -4,4 +4,9 @@ package com.demo;
  * Created by Administrator on 2017/7/17 0017.
  */
 public class AAA {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
