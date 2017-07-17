1bb57524cb02e6d6a3bafd0e179e8f407744a970
chenxianguo <chenxianguo@wtoip.com>
2017-07-17 13:35:10 +0800

    测试。。。。。

---
+++ src/main/java/com/demo/AAA.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+/**
+ * Created by Administrator on 2017/7/17 0017.
+ */
+public class AAA {
+}
---
+++ src/main/java/com/demo/DemoMain.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * Created by Administrator on 2017/7/17 0017.
+ */
+public class DemoMain {
+
+    public static void main(String args[]){
+
+    }
+
+}
